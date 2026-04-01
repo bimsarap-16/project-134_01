cba0f4afe14422eb312c66707cb388c4f989d7b1
bimsara <132643253+maxak9@users.noreply.github.com>
2026-04-01 19:25:40 +0530

    Configure MongoDB Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const dns = require("dns");
+require("dotenv").config();
+
+dns.setDefaultResultOrder('ipv4first');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
